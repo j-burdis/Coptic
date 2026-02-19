3840013a346d789771b392e1344ba22be8cb41b1
Jonathan Burdis <jonnyburdis@hotmail.com>
2026-02-19 20:33:02 +0000

    update active admin pages for collections

--- app/admin/collections.rb
+++ app/admin/collections.rb
@@ -1,18 +1,73 @@
 ActiveAdmin.register Collection do
   permit_params :name, :slug, :location, :region, :description, :website, :published
-end
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :name, :slug, :location, :region, :description, :website, :published
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name, :slug, :location, :region, :description, :website, :published]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+  filter :name
+  filter :location
+  filter :region
+
+  form do |f|
+    f.semantic_errors
+
+    columns do
+      column do
+        f.inputs "Collection details" do
+          f.input :name
+          f.input :slug
+          f.input :location
+          f.input :region
+          f.input :website
+          f.input :published
+        end
+      end
+
+      column do
+        f.inputs "Additional details" do
+          f.input :description,
+                  as: :text,
+                  input_html: { rows: 6 }
+        end
+      end
+    end
+    f.actions
+  end
+
+  index do
+    selectable_column
+    id_column
+  
+    column :name
+    column :location
+    column :region
+    column :website
+    actions
+  end
+
+  show do
+    columns do
+      column do
+        panel "Details" do
+          attributes_table_for collection do
+            row :name
+            row :slug
+            row :location
+            row :region
+            row :website
+            row :published do
+              status_tag(collection.published ? 'Yes' : 'No', class: (collection.published ? 'yes' : 'no'))
+            end
+          end
+        end
+      end
+
+      column do
+        panel "Description" do
+          attributes_table_for collection do
+            row :description
+            row :created_at
+            row :updated_at
+          end
+        end
+      end
+    end
+  end
+end

6e486b59bc1d0ab280a1e1b168e879448383b964
Jonathan Burdis <jonnyburdis@hotmail.com>
2026-03-27 08:24:28 +0000

    improve navbar dropdown transition style

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -1,8 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["mobileOverlay", "mobileSidebar"]
-  static values = { duration: { type: Number, default: 500 } }
+  static values = { duration: { type: Number, default: 300 } }
 
   connect() {
     this.currentDropdown = null
@@ -13,12 +12,11 @@ export default class extends Controller {
     this.removeGlobalListeners()
   }
 
-  // Desktop dropdown toggle
   toggle(event) {
     event.stopPropagation()
     const menu = event.currentTarget.nextElementSibling
 
-    if (this.currentDropdown === menu && !menu.classList.contains('hidden')) {
+    if (this.currentDropdown === menu && menu.style.maxHeight !== "0px" && menu.style.maxHeight !== "") {
       this.close(menu)
       this.currentDropdown = null
       return
@@ -34,15 +32,11 @@ export default class extends Controller {
 
   open(menu) {
     menu.classList.remove('hidden')
-    menu.offsetHeight // Force reflow
-    menu.classList.remove('opacity-0', 'max-h-0')
-    menu.classList.add('opacity-100', 'max-h-[1000px]')
+    menu.style.maxHeight = menu.scrollHeight + "px"
   }
 
   close(menu) {
-    menu.classList.remove('opacity-100', 'max-h-[1000px]')
-    menu.classList.add('opacity-0', 'max-h-0')
-    
+    menu.style.maxHeight = "0px"
     setTimeout(() => {
       menu.classList.add('hidden')
     }, this.durationValue)
@@ -57,16 +51,9 @@ export default class extends Controller {
 
   closeOnEscape(event) {
     if (event.key === 'Escape') {
-      // Close desktop dropdown
       if (this.currentDropdown) {
         this.close(this.currentDropdown)
         this.currentDropdown = null
-      }
-      
-      // Close mobile menu if open
-      if (this.hasMobileSidebarTarget && 
-          !this.mobileSidebarTarget.classList.contains('translate-x-full')) {
-        this.toggleMobile()
       }
     }
   }
@@ -74,7 +61,6 @@ export default class extends Controller {
   setupGlobalListeners() {
     this.clickOutsideHandler = this.closeOnClickOutside.bind(this)
     this.escapeHandler = this.closeOnEscape.bind(this)
-    
     document.addEventListener('click', this.clickOutsideHandler)
     document.addEventListener('keydown', this.escapeHandler)
   }

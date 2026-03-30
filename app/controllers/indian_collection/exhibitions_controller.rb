e30271f9f5c5c04e5beb87a4c354b39de6e8e421
Jonathan Burdis <jonnyburdis@hotmail.com>
2026-03-30 08:27:12 +0100

    format exhibition list for indian collection

--- app/controllers/indian_collection/exhibitions_controller.rb
+++ app/controllers/indian_collection/exhibitions_controller.rb
@@ -3,7 +3,32 @@ module IndianCollection
     layout 'indian_collection'
 
     def list
-      @exhibition_list = IndianCollectionExhibitionList.published.first
+      raw_list = IndianCollectionExhibitionList.published.first
+      
+      if raw_list&.content.present?
+        @decades = parse_exhibition_list(raw_list.content)
+      end
+    end
+
+    private
+
+    def parse_exhibition_list(content)
+      decades = {}
+      current_decade = nil
+
+      content.each_line do |line|
+        line = line.strip
+        next if line.blank?
+
+        if line.match?(/^\d{4}s$/)
+          current_decade = line
+          decades[current_decade] = []
+        elsif current_decade
+          decades[current_decade] << line
+        end
+      end
+
+      decades
     end
   end
 end
